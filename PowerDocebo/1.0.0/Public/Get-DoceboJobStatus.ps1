62aae39ff86e5148c1a8cd44e6255b8d12ad530b
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2021-04-14 16:01:07 -0400

    Module Created
    Module Created

---
+++ PowerDocebo/1.0.0/Public/Get-DoceboJobStatus.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.Synopsis
+    Get Latest Job Status
+
+.EXAMPLE
+    Get-DoeboJobStatus -CompURL "verabradleyu" -Token "Token" -AuthorID "AuthorID"
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 4/14/2021
+
+#>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Function Get-DoceboJobStatus {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Companies URL.")]
+        [string[]]$CompURL,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Token.")]
+        [string[]]$Token,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Job Author ID.")]
+        [string[]]$AuthorID
+
+    )
+
+    $Uri = @{
+        "Manage" = "https://$CompURL.docebosaas.com/manage/v1/job?id_author=$AuthorID&page_size=1"
+    }
+
+    $Header = @{
+        'Authorization'   = "Bearer $Token"
+        'Content-Type'  = 'application/json';
+    }  
+
+    $Manage = Invoke-RestMethod -Method GET -Uri $Uri.Manage -Headers $Header
+    Write-Output $Manage.data.jobs
+}
