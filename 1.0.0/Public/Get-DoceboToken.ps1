db37ccb41cb0f3083e464cd516edd04911b32470
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2021-04-14 16:02:27 -0400

    Add files via upload

---
+++ 1.0.0/Public/Get-DoceboToken.ps1
@@ -0,0 +1,65 @@
+﻿<#
+.Synopsis
+    Generates Token for Docebo API.
+
+.EXAMPLE
+    Get-DoeboToken -CompURL "verabradleyu" -ClientID "XXX" -ClientSecret "ClientSecret" -Username "Username" -Password "Password"
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 4/14/2021
+
+#>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Function Get-DoceboToken {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Companies URL.")]
+        [string[]]$CompURL,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your ClientID.")]
+        [string[]]$ClientID,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your ClientSecret.")]
+        [string[]]$ClientSecret,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Username.")]
+        [string[]]$Username,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Password.")]
+        [string[]]$Password
+
+    )
+
+    $Uri = @{
+        "Token" = "https://$CompURL.docebosaas.com/oauth2/token"
+    }
+
+    $Body = @{'grant_type' = "password";} 
+    $Body += @{'client_id'   = "$ClientID";}
+    $Body += @{'client_secret'   = "$ClientSecret";}
+    $Body += @{'scope'      = "api"}
+    $Body += @{'username'      = "$Username"}
+    $Body += @{'password'      = "$Password"}
+
+    $JsonBody = ConvertTo-Json $Body
+
+    $Header = @{
+        'Content-Type'  = 'application/json';
+    }
+
+    $Token = Invoke-RestMethod -Method POST -Uri $Uri.Token -Body $JsonBody -Headers $Header
+
+    Write-Output $Token.access_token
+}
---
+++ 1.0.0/Public/New-DoceboCourse.ps1
@@ -0,0 +1,91 @@
+﻿<#
+.Synopsis
+    Get All Courses from Docebo API.
+
+.EXAMPLE
+    New-DoeboCourse -CompURL "verabradleyu" -Token "Token" -Name "Test" -Code "T-TTTT" -Description "<p>test test</p>" -CourseType "elearning" -Language "english" -LanguageLabel "English" -Status "published" 
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 4/14/2021
+
+#>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Function New-DoceboCourse {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Companies URL.")]
+        [string[]]$CompURL,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Token.")]
+        [string[]]$Token,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Course Name.")]
+        [string[]]$Name,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Course Code.")]
+        [string[]]$Code,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Course Description.")]
+        [string[]]$Description,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Course Type.")]
+        [string[]]$CourseType,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Course Language.")]
+        [string[]]$Language,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Course Language Label.")]
+        [string[]]$LanguageLabel,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Docebo Course Status.")]
+        [string[]]$Status
+
+    )
+
+    $Uri = @{
+        "Courses" = "https://$CompURL.docebosaas.com/learn/v1/courses"
+    }
+
+    $Header = @{
+        'Authorization'   = "Bearer $Token"
+        'Content-Type'  = 'application/json';
+    }
+    
+    $Body = @{"name" = "$Name"} 
+    $Body += @{"code" = "$Code"}
+    $Body += @{"description" = "$Description"}
+    $Body += @{"course_type" = "$CourseType"}
+    $Body += @{"selling" = "False"}
+    $Body += @{"language" = "$Language"}
+    $Body += @{"language_label" = "$LanguageLabel"}
+    $Body += @{"price" = "0"}
+    $Body += @{"is_new" = "0"}
+    $Body += @{"is_opened" = "1"}
+    $Body += @{"duration" = "0"}
+    $Body += @{"status" = "$Status"}
+
+    $postData = ConvertTo-Json $Body  
+
+    $Courses = Invoke-RestMethod -Method POST -Uri $Uri.Courses -Body $postData -Headers $Header
+    Write-Output $Courses.data.items
+}
